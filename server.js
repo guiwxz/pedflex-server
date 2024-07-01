37fef5d3fd8b11675c34b11a8340c0ba736d28f3
bernardo damiani <bmdamiani17@gmail.com>
2024-07-01 14:58:13 -0300

    feat: add lista negociacao

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-import express from "express";
-import cors from "cors";
-import { list2 } from "./mocks/list.js";
-import { header } from "./mocks/header.js";
-import { lista, headerStevan } from "./mocks/stevan.js";
-import { combo } from "./mocks/combo.js";
-import { lucasHeader } from "./mocks/lucasHeader.js";
-import { lucasList } from "./mocks/lucasList.js";
+import express from 'express';
+import cors from 'cors';
+import { list2 } from './mocks/list.js';
+import { header } from './mocks/header.js';
+import { lista, headerStevan } from './mocks/stevan.js';
+import { combo } from './mocks/combo.js';
+import { lucasHeader } from './mocks/lucasHeader.js';
+import { lucasList } from './mocks/lucasList.js';
+import { ListaNegociacao } from './mocks/lucasListaNegociacao.js';
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
-server.use("/lista-produto", (req, res) => {
+server.use('/lista-produto', (req, res) => {
   // const { cd_condicao } = req.query
 
   // const valueMultiplier = cd_condicao % 2 == 0 ? 0.2 : 1
@@ -25,20 +26,25 @@ server.use("/lista-produto", (req, res) => {
   //   }))
   // })))
 
-  return res.json(lucasList)
-
+  return res.json(lucasList);
 });
 
-server.use("/header", (req, res) => {
-
-  return res.json(lucasHeader)
+server.use('/header', (req, res) => {
+  return res.json(lucasHeader);
 });
 
-server.use("/combo", (req, res) => {
+server.use('/lista-negociacao', (req, res) => {
+  return res.json(ListaNegociacao);
+});
 
-  return res.json(combo)
+server.use('/combo', (req, res) => {
+  return res.json(combo);
 });
 
 server.listen(process.env.PORT || 3333, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${process.env.PORT || 3333}`);
+  console.log(
+    `⚡️[server]: Server is running at https://localhost:${
+      process.env.PORT || 3333
+    }`
+  );
 });
